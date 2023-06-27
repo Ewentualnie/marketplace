de4973fecb862929601d243a9b40fa7a28dd2b66
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-06-27 11:30:57 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'sql_db',
-      port: 3306,
-      username: 'admin',
-      password: 'root',
-      database: 'marketplace',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      //   {
+      //   type: 'mysql',
+      //   host: 'sql_db',
+      //   port: 3306,
+      //   username: 'admin',
+      //   password: 'root',
+      //   database: 'marketplace',
+      //   autoLoadEntities: true,
+      //   synchronize: true,
+      // }
+      {
+        type: 'postgres',
+        host: 'dpg-cid99aunqqlb62hi8ic0-a',
+        port: 5432,
+        username: 'admin',
+        password: '4JaXiFxFOoaGvN5ssDw1L9GIodADOEP5',
+        database: 'marketplace_fs5r',
+        autoLoadEntities: true,
+        synchronize: true,
+      },
+    ),
     UsersModule,
   ],
   controllers: [AppController],
