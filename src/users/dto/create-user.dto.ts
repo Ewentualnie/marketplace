c8f357651a4424defdcaa51a1acfa61cb6826551
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-07-25 22:27:45 +0300

    add decorators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   name: string;
 }
