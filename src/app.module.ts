c7c4ec45a608c77c1d56d449290d9c96f1e60cc4
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-07-25 12:46:01 +0300

    add run with local setings

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,27 @@ import { AdvertisementsModule } from './advertisements/advertisements.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'dpg-cid99aunqqlb62hi8ic0-a',
-      port: 5432,
-      username: 'admin',
-      password: '4JaXiFxFOoaGvN5ssDw1L9GIodADOEP5',
-      database: 'marketplace_fs5r',
+      // type: 'postgres',
+      // host: 'dpg-cid99aunqqlb62hi8ic0-a',
+      // port: 5432,
+      // username: 'admin',
+      // password: '4JaXiFxFOoaGvN5ssDw1L9GIodADOEP5',
+      // database: 'marketplace_fs5r',
+
       // type: 'postgres',
       // host: 'postgres_db',
       // port: 5432,
       // username: 'admin',
       // password: 'root',
       // database: 'marketplace',
+
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'root',
+      database: 'marketplace',
+
       autoLoadEntities: true,
       synchronize: true,
     }),
