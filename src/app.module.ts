d201cd969527a63d3ac703165c5df89f9a73c05d
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-07-27 14:07:30 +0300

    add advert entity and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdvertisementsModule } from './advertisements/advertisements.module';
 import { User } from './users/entities/user.entity';
+import { AdvertModule } from './advert/advert.module';
+import { Advert } from './advert/entities/advert.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST || 'localhost',
-      // host: 'postgres_db',
+      // host: 'postgres_db',    //need for run in local docker
       port: 5432,
       username: process.env.POSTGRES_USER || 'admin',
       password: process.env.POSTGRES_PASS || 'root',
       database: process.env.POSTGRES_DATABASE || 'marketplace',
 
-      entities: [User],
+      entities: [User, Advert],
       synchronize: true,
     }),
     UsersModule,
-    AdvertisementsModule,
+    AdvertModule,
   ],
   controllers: [AppController],
   providers: [AppService],
