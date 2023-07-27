69194dc05f6f88b68895cb77eaa910fee25019e8
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-07-27 19:48:20 +0300

    add entity to typeorm

--- src/advert/advert.module.ts
+++ src/advert/advert.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdvertService } from './advert.service';
 import { AdvertController } from './advert.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Advert } from './entities/advert.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Advert])],
   controllers: [AdvertController],
-  providers: [AdvertService]
+  providers: [AdvertService],
 })
 export class AdvertModule {}
