35b674c5e3b19fd26a0b74ddba82ed4f563c0f93
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-08-01 23:34:33 +0300

    add validation params

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,19 @@
 import { IsNotEmpty, IsEmail } from 'class-validator';
+import { IsValidEmail } from 'src/auth/decorators/email_validator';
+import { IsValidName } from 'src/auth/decorators/name-validator';
+import { IsValidPassword } from 'src/auth/decorators/pass-validator';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @IsValidEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsValidPassword()
   password: string;
 
   @IsNotEmpty()
+  @IsValidName()
   name: string;
 }
