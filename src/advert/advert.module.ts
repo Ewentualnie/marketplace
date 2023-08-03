369f8a403a327c413d6a271294485d29cf75a8a3
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-08-03 16:20:01 +0300

    add userService

--- src/advert/advert.module.ts
+++ src/advert/advert.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Advert } from './entities/advert.entity';
 import { Hobby } from './entities/hobby.entity';
 import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Advert, Hobby])],
   controllers: [AdvertController],
-  providers: [AdvertService],
+  providers: [AdvertService, UsersService],
 })
 export class AdvertModule {}
