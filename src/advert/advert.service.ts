62350c5e2ca2b300d146a1925e9b1fede15f8579
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-08-03 16:23:57 +0300

    add possibility create advert, get advert, get all adverts

--- src/advert/advert.service.ts
+++ src/advert/advert.service.ts
@@ -1,29 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAdvertDto } from './dto/create-advert.dto';
 import { UpdateAdvertDto } from './dto/update-advert.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Advert } from './entities/advert.entity';
 import { Repository } from 'typeorm';
 import { Hobby } from './entities/hobby.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AdvertService {
   constructor(
     @InjectRepository(Advert) private advertRepository: Repository<Advert>,
     @InjectRepository(Hobby) private hobbyRepository: Repository<Hobby>,
+    private userService: UsersService,
   ) {}
 
   async create(userId: number, createAdvertDto: CreateAdvertDto) {
-    // const advert = await this.advertRepository.findOneBy({
-    //   user: { where: { id: userId } },
-    // });
+    const user = await this.userService.findOne(userId);
+
+    if (!user) {
+      throw new NotFoundException(`User wirh id ${userId} not found`);
+    }
+
     const newAdvert = this.advertRepository.create(createAdvertDto);
-    const savedAdvert = await this.advertRepository.save(newAdvert);
-    return this.advertRepository.create(createAdvertDto);
+    newAdvert.user = user;
+    this.userService.update(userId, newAdvert);
+    return await this.advertRepository.save(newAdvert);
   }
 
   findAll() {
-    return this.advertRepository.find();
+    return this.advertRepository.find({ relations: ['user'] });
   }
 
   findOne(id: number) {
