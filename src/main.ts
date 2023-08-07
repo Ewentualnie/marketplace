1d8c14ab3827f9c88c1d086dcb2963ad26a85c14
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-08-07 14:25:07 +0300

    add global validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   app.use(
     session({
       name: 'session_id',
