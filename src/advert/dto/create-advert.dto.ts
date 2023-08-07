4f703c7f68d7e8285a9608bc8390a60e7cd889d0
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-08-07 14:26:34 +0300

    add decorators to validate price

--- src/advert/dto/create-advert.dto.ts
+++ src/advert/dto/create-advert.dto.ts
@@ -1,12 +1,18 @@
-import { IsNotEmpty } from 'class-validator';
-import { IsValidPrice } from 'src/utils/validators/price-validator';
+import { Transform, Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, Matches } from 'class-validator';
 
 export class CreateAdvertDto {
   @IsNotEmpty()
   shortDescription: string;
 
   @IsNotEmpty()
-  @IsValidPrice()
+  @IsNumber()
+  @Matches(/^(?!-0?$)(?!0*$)(\d{1,4}(\.\d{1,2})?|\.\d{1,2})$/, {
+    message:
+      'Invalid price format. Price must be a number following pattern  XXXX.XX or XXXXX more then 0.',
+  })
+  @Transform(({ value }) => parseFloat(value))
+  @Type(() => Number)
   price: number;
 
   imagePath: string;
