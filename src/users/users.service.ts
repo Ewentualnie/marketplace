854290d37536179fbc498afd3c2d1dbc35c86d47
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-09-29 13:16:23 +0300

    add async await and delete user methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Advert } from 'src/advert/entities/advert.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -11,33 +12,38 @@ export class UsersService {
     public usersRepository: Repository<User>,
   ) {}
 
-  findByEmail(email: string) {
-    const user = this.usersRepository.findOne({
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({
       where: { email: email },
     });
     if (user) return user;
-    throw new HttpException(
-      `User with email ${email} not found`,
-      HttpStatus.BAD_REQUEST,
-    );
+    throw new BadRequestException(`User with email ${email} not found`);
   }
 
-  findAll() {
-    return this.usersRepository.find({ relations: ['advert'] });
+  async findAll() {
+    return await this.usersRepository.find({ relations: ['advert'] });
   }
 
-  findOne(id: number) {
-    return this.usersRepository.findOne({
+  async findOne(id: number) {
+    return await this.usersRepository.findOne({
       where: { id },
       relations: ['advert'],
     });
   }
 
-  updateAdvert(id: number, advert: Advert) {
-    return this.usersRepository.update(id, { advert: advert });
+  async updateAdvert(id: number, advert: Advert) {
+    return await this.usersRepository.update(id, { advert: advert });
   }
 
-  remove(id: number) {
-    return this.usersRepository.delete(id);
+  async softUserDelete(id: number) {
+    return await this.usersRepository.update(id, { isDeleted: true });
+  }
+
+  async hardUserDelete(id: number) {
+    return await this.usersRepository.remove((await this.findOne(id))[0]);
+  }
+
+  async updateUserInfo(id: number, updateUserDto: UpdateUserDto) {
+    return 'not implemented yet';
   }
 }
