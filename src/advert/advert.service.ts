dd43bd37778f8367d8ac417e9a3243f10a1da792
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-09-29 13:13:30 +0300

    add some methods

--- src/advert/advert.service.ts
+++ src/advert/advert.service.ts
@@ -8,13 +8,7 @@ import { CreateAdvertDto } from './dto/create-advert.dto';
 import { UpdateAdvertDto } from './dto/update-advert.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Advert } from './entities/advert.entity';
-import {
-  Connection,
-  EntityMetadata,
-  Repository,
-  getConnection,
-  getMetadataArgsStorage,
-} from 'typeorm';
+import { Repository } from 'typeorm';
 import { Hobby } from './entities/hobby.entity';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
@@ -30,12 +24,9 @@ export class AdvertService {
     private languageRepository: Repository<Language>,
     private userService: UsersService,
     public jwtService: JwtService,
-    private connection: Connection,
   ) {}
 
-  async create(createAdvertDto: CreateAdvertDto, accesToken: string) {
-    const user = await this.getUserFromToken(accesToken);
-
+  async create(createAdvertDto: CreateAdvertDto, user: User) {
     if (user.advert != null) {
       throw new ConflictException(`User cannot have more then one advert.`);
     }
@@ -56,38 +47,17 @@ export class AdvertService {
     return savedAdvert;
   }
 
-  findAll(query: any) {
-    // const { sort } = query;
-    // const sortFields = [];
-
-    // if (sort) {
-    //   sort.split(',').forEach((item) => {
-    //     const [sortBy, sortDirection] = item.split(':');
-    //     sortFields.push({ [sortBy]: sortDirection });
-    //   });
-    // }
-
-    // const order = {};
-
-    // for (const sortField of sortFields) {
-    //   const sortBy = Object.keys(sortField)[0];
-    //   const sortDirection = sortField[sortBy];
-    //   order[sortBy] = sortDirection as 'ASC' | 'DESC';
-    // }
-
-    // Object.keys(order).forEach(async (val) => {
-    //   const res = await this.isOrderValid(val);
-    //   console.log(res);
-    //   if (!res) {
-    //     throw new BadRequestException(
-    //       `sort parameter ${val} does not exist in the advert database`,
-    //     );
-    //   }
-    // });
-
-    return this.advertRepository.find({
+  async findAllowedAdverts(query: any) {
+    return await this.advertRepository.find({
       relations: ['user', 'hobbies', 'spokenLanguages', 'teachingLanguages'],
+      where: { isDeleted: false },
       // order,
+    });
+  }
+
+  async findAllAdverts() {
+    return await this.advertRepository.find({
+      relations: ['user', 'hobbies', 'spokenLanguages', 'teachingLanguages'],
     });
   }
 
@@ -101,29 +71,40 @@ export class AdvertService {
     )[0];
 
     if (!advert) {
-      throw new NotFoundException('Page not found');
+      throw new NotFoundException('Advert not found');
     }
     return advert;
   }
 
-  update(id: number, updateAdvertDto: UpdateAdvertDto, user: User) {
-    return this.advertRepository.update(id, updateAdvertDto);
+  async update(id: number, updateAdvertDto: UpdateAdvertDto, user: User) {
+    // return await this.advertRepository.update(id, updateAdvertDto);
+    return 'not implement yet';
   }
 
-  remove(id: number, user: User) {
-    return this.advertRepository.delete(id);
-  }
-
-  async getUserFromToken(accessToken: string): Promise<User> {
-    const jwt = accessToken.replace('Bearer', '').trim();
-    const userId = this.jwtService.decode(jwt).sub;
-    const user = await this.userService.findOne(userId);
-
-    if (!user) {
-      throw new NotFoundException(`User with id ${userId} not found`);
+  async removeOwnAdvert(user: User) {
+    if (user.advert == null) {
+      throw new BadRequestException(`User with ID ${user.id} has no advert`);
     }
+    return await this.softDeleteAdvert(user.advert.id);
+  }
 
-    return user;
+  async restoreOwnAdvert(user: User) {
+    if (user.advert == null) {
+      throw new BadRequestException(`User with ID ${user.id} has no advert`);
+    }
+    return await this.restoreAdvert(user.advert.id);
+  }
+
+  async softDeleteAdvert(id: number) {
+    return await this.advertRepository.update(id, {
+      isDeleted: true,
+    });
+  }
+
+  async restoreAdvert(id: number) {
+    return await this.advertRepository.update(id, {
+      isDeleted: false,
+    });
   }
 
   async getHobbies(hobbies: Hobby[]): Promise<Hobby[]> {
@@ -156,13 +137,5 @@ export class AdvertService {
         );
       }),
     );
-  }
-
-  async isOrderValid(order: string): Promise<boolean> {
-    const entityMetadata = this.connection.getMetadata(Advert);
-    const columnNames = entityMetadata.columns.map(
-      (column) => column.databaseName,
-    );
-    return columnNames.includes(order);
   }
 }
