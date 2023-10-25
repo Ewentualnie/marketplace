ac51a3c4cca19f4eafca605c09d6e47d07892fbd
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-10-25 15:28:34 +0300

    add exception when user not found starting addFeedback method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,12 +4,14 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Advert } from 'src/advert/entities/advert.entity';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateFeedback } from './dto/add-feedback.dto';
+import { FeedBack } from './entities/feedback.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User)
-    public usersRepository: Repository<User>,
+    @InjectRepository(User) public usersRepository: Repository<User>,
+    @InjectRepository(FeedBack) public feedbackRepository: Repository<FeedBack>,
   ) {}
 
   async findByEmail(email: string) {
@@ -25,10 +27,12 @@ export class UsersService {
   }
 
   async findOne(id: number) {
-    return await this.usersRepository.findOne({
+    const user = await this.usersRepository.findOne({
       where: { id },
-      relations: ['advert'],
+      relations: ['advert', 'feedbacks', 'writtenFeedbacks'],
     });
+    if (user) return user;
+    throw new BadRequestException(`User with id ${id} not found`);
   }
 
   async updateAdvert(id: number, advert: Advert) {
@@ -45,5 +49,38 @@ export class UsersService {
 
   async updateUserInfo(id: number, updateUserDto: UpdateUserDto) {
     return 'not implemented yet';
+  }
+
+  async addFeedback(
+    userId: number,
+    currentUserId: number,
+    feedback: CreateFeedback,
+  ) {
+    const user = await this.findOne(userId);
+    const currentUser = await this.findOne(currentUserId);
+    const newFeedback: FeedBack = await this.feedbackRepository.create(
+      feedback,
+    );
+
+    // console.log('before', newFeedback);
+    user.feedbacks.push(newFeedback);
+    currentUser.writtenFeedbacks.push(newFeedback);
+    // add new feedback to user, working good, need to save changes in user entity
+    // add new writtenfeedback to current user, working good, need to save changes in current user entity
+    this.usersRepository.update(user, { feedbacks: user.feedbacks });
+    // не зберігається поле фідбеків
+    console.log(user);
+
+    newFeedback.toUser = user;
+    // newFeedback.fromUsers.push(currentUser);
+    //тут поля фромюзерс взагалі нема
+
+    console.log(newFeedback);
+
+    // this.feedbackRepository.save(newFeedback);
+
+    // throw new Error('Method not implemented.');
+
+    return null;
   }
 }
