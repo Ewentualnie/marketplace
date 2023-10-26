f569ecd748b424bde7981196d73146d6c4ddd60d
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-10-26 11:43:53 +0300

    remove unnecessary log

--- src/utils/decorators/get-user-id.decorator.ts
+++ src/utils/decorators/get-user-id.decorator.ts
@@ -3,7 +3,6 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 export const GetCurrentUserId = createParamDecorator(
   (data: undefined, contex: ExecutionContext): number => {
     const request = contex.switchToHttp().getRequest();
-    console.log('request', request);
     return request.user['sub'];
   },
 );
