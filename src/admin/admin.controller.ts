7038cc3cba3495bbc9f3288fe869596740324102
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-11-07 11:33:57 +0200

    remove decorators

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -6,20 +6,12 @@ import {
   Param,
   ParseIntPipe,
   Patch,
-  Query,
   UseGuards,
 } from '@nestjs/common';
-import { User } from 'src/users/entities/user.entity';
-import { GetCurrentUser } from 'src/utils/decorators/get-user.decorator';
 import { AdminService } from './admin.service';
 import { UpdateUserDto } from 'src/users/dto/update-user.dto';
 import { UpdateAdvertDto } from 'src/advert/dto/update-advert.dto';
-import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Admin')
@@ -29,28 +21,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @ApiOperation({ summary: 'Get information of all users by admin' })
-  @ApiResponse({
-    status: 200,
-    description: 'List of users and adverts',
-    content: {
-      'application/json': {
-        example: {
-          adverts: [],
-          users: [
-            {
-              id: 1,
-              email: 'example@email.com',
-              name: 'Name',
-              role: 'role',
-              isDeleted: 'boolean',
-              advert: 'null',
-            },
-          ],
-        },
-      },
-    },
-  })
   @Get('users')
   async getUsers() {
     return await this.adminService.getUsers();
@@ -61,7 +31,6 @@ export class AdminController {
     return await this.adminService.getAdverts();
   }
 
-  @ApiOperation({ summary: 'Deleting user by admin' })
   @Delete('users/:id')
   async deleteUser(@Param('id', ParseIntPipe) id: number) {
     return await this.adminService.deleteUser(id);
