dd26b940021d7f9d634f1ce3643316ce5455002b
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-11-07 11:02:01 +0200

    remove checking role in methods

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,14 +1,8 @@
-import {
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AdvertService } from 'src/advert/advert.service';
 import { UpdateAdvertDto } from 'src/advert/dto/update-advert.dto';
 import { UpdateUserDto } from 'src/users/dto/update-user.dto';
-import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
-import { Role } from 'src/utils/role.enum';
 
 @Injectable()
 export class AdminService {
@@ -17,49 +11,31 @@ export class AdminService {
     private readonly usersService: UsersService,
   ) {}
 
-  async getAll(admin: User, query: any) {
-    if (admin.role != Role.Admin) {
-      this.errorExeption('this page');
-    }
-    const adverts = await this.advertService.findAllowedAdverts(query);
-    const users = await this.usersService.findAll();
-    return { adverts, users };
+  async getUsers() {
+    return await this.usersService.findAll();
   }
 
-  async deleteUser(admin: User, id: number) {
-    if (admin.role != Role.Admin) {
-      this.errorExeption('delete users');
-    }
+  async getAdverts() {
+    return await this.advertService.findAllAdverts();
+  }
+
+  async deleteUser(id: number) {
     const user = await this.usersService.findOne(id);
-
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-
     if (user.advert != null) {
       await this.advertService.softDeleteAdvert(user.advert.id);
     }
     return await this.usersService.softUserDelete(id);
   }
 
-  async deleteAdvert(admin: User, id: number) {
-    if (admin.role != Role.Admin) {
-      this.errorExeption('delete adverts');
-    }
+  async deleteAdvert(id: number) {
     return await this.advertService.softDeleteAdvert(id);
   }
 
-  async editUser(admin: User, id: number, updateUserDto: UpdateUserDto) {
+  async editUser(userId: number, updateUserDto: UpdateUserDto) {
     return 'not implement yet';
   }
 
-  async editAdvert(admin: User, id: number, updateAdvertDto: UpdateAdvertDto) {
+  async editAdvert(userId: number, updateAdvertDto: UpdateAdvertDto) {
     return 'not implement yet';
-  }
-
-  errorExeption(message: string) {
-    throw new ForbiddenException(
-      `Only administrators have access to ${message}`,
-    );
   }
 }
