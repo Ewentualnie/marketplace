21bd6e23ec56f4790cfe068cca2d385a253eede0
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-11-10 22:03:41 +0200

    add transform to price, change field name and type

--- src/models/dto/create-advert.dto.ts
+++ src/models/dto/create-advert.dto.ts
@@ -1,17 +1,19 @@
 import { IsNotEmpty, IsNumber, IsPositive, Max } from 'class-validator';
 import { Language } from '../language.entity';
+import { Transform } from 'class-transformer';
 
 export class CreateAdvertDto {
   @IsNotEmpty()
   description: string;
 
   @IsNotEmpty()
+  @Transform(({ value }) => parseInt(value, 10))
   @IsNumber()
   @IsPositive({ message: 'Price must be greater than 0.' })
   @Max(9999, { message: 'Price must be less than or equal to 9999.' })
   price: number;
 
-  imagePath: string;
+  image: any;
 
   @IsNotEmpty()
   spokenLanguages: Language[];
