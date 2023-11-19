b7728731ad4da3fbe5e6948cffb5d760ce717afe
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-11-19 22:46:10 +0200

    add public route to get Countries from db

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { Public } from './utils/decorators/public.decorator';
 import { Specialization } from './models/specialization.entity';
 import { Language } from './models/language.entity';
+import { Country } from './models/country.entity';
 
 @Controller()
 export class AppController {
@@ -10,13 +11,19 @@ export class AppController {
 
   @Public()
   @Get('languages')
-  getLanguages(): Promise<Language[]> {
+  async getLanguages(): Promise<Language[]> {
     return this.appService.getLanguages();
   }
 
   @Public()
   @Get('specializations')
-  getSpecializations(): Promise<Specialization[]> {
+  async getSpecializations(): Promise<Specialization[]> {
     return this.appService.getSpecializations();
+  }
+
+  @Public()
+  @Get('countries')
+  async getCountries(): Promise<Country[]> {
+    return this.appService.getCountries();
   }
 }
