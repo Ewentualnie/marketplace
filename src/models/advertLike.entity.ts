6608e0f0142b1fa4fc526f2128138953e9ae57ca
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-07-02 20:22:15 +0300

    exclude likes id

--- src/models/advertLike.entity.ts
+++ src/models/advertLike.entity.ts
@@ -1,10 +1,12 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 import Advert from './advert.entity';
 import User from './user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity({ name: 'advert_likes' })
 export class AdvertLike {
   @PrimaryGeneratedColumn()
+  @Exclude()
   id: number;
 
   @ManyToOne(() => User, (user) => user.likes)
