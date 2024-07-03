9535a63a308ed2ade5a3bb4be11426b988fa67e4
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-07-03 20:55:27 +0300

    add possibility to add only not existing time slots to user

--- src/schedule/timeslots.service.ts
+++ src/schedule/timeslots.service.ts
@@ -24,26 +24,30 @@ export class TimeSlotsService {
   }
 
   async addTimeSlots(timeslotsRequestDto: TimeSlotsRequestDto, id: number) {
-    const user = await this.usersRepository.findOne({
+    let user = await this.usersRepository.findOne({
       where: { id },
       relations: ['timeslots'],
     });
     if (!user) {
       throw new BadRequestException(`User with id ${id} does not exist`);
     }
-    const timeslots: TimeSlot[] = [];
-    for (const timeslot of timeslotsRequestDto.timeslots) {
-      timeslots.push(
-        this.timeslotRepository.create({
-          start: timeslot.start,
-          end: timeslot.end,
-          user,
-        }),
-      );
-    }
-    const savedSlots = await this.timeslotRepository.save(timeslots);
+
+    const existingDates = user.timeslots.map((slot) =>
+      slot.start.toDateString(),
+    );
+
+    const uniqueTimeSlots = timeslotsRequestDto.timeslots.filter((slot) => {
+      const startDateString = new Date(slot.start).toDateString();
+      if (existingDates.includes(startDateString)) {
+        return false;
+      }
+      existingDates.push(startDateString);
+      return true;
+    });
+
+    const savedSlots = await this.timeslotRepository.save(uniqueTimeSlots);
     user.timeslots.push(...savedSlots);
-    await this.usersRepository.save(user);
-    return timeslots;
+    user = await this.usersRepository.save(user);
+    return user.timeslots;
   }
 }
