1bd42a45915548503ed9eb1883459e7d2f16ae54
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-07-13 22:12:14 +0300

    remove unnecessary validation

--- src/models/dto/timeslots-request.dto.ts
+++ src/models/dto/timeslots-request.dto.ts
@@ -1,6 +1,5 @@
 import { Type } from 'class-transformer';
 import { IsArray, ValidateNested } from 'class-validator';
-import { IsTimeSlotsValid } from 'src/utils/validators/timeslots_validator';
 import { IsUniqueTimeSlots } from 'src/utils/validators/unique_timeslots_validator';
 import TimeSlot from '../timeslot.entity';
 
@@ -8,7 +7,6 @@ export class TimeSlotsRequestDto {
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => TimeSlot)
-  @IsTimeSlotsValid()
   @IsUniqueTimeSlots()
   timeslots: TimeSlot[];
 }
