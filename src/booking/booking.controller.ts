470d7e3531b72af72b28bd2b9aea0787065a6ce4
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-07-13 22:10:31 +0300

    add possibility to creating bookings by timeslots

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -2,12 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { GetCurrentUserId } from 'src/utils/decorators/get-user-id.decorator';
 import AcceptBookingDto from 'src/models/dto/accept-booking.dto';
+import TimeSlotsRequestDto from 'src/models/dto/timeslots-request.dto';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingservice: BookingService) {}
 
   @Post('')
+  addBooking(
+    @Body() timeslotsDto: TimeSlotsRequestDto,
+    @GetCurrentUserId() userId: number,
+  ) {
+    return this.bookingservice.addBookings(timeslotsDto, userId);
+  }
+
+  @Post('accept')
   acceptBooking(
     @Body() acceptBookingDto: AcceptBookingDto,
     @GetCurrentUserId() studentId: number,
