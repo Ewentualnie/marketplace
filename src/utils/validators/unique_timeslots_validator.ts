1b801a0b359c1053b1f6bb20ea5c612a2af19b8f
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-07-13 22:09:55 +0300

    add checking timeslots

--- src/utils/validators/unique_timeslots_validator.ts
+++ src/utils/validators/unique_timeslots_validator.ts
@@ -10,21 +10,45 @@ import TimeSlotDto from 'src/models/dto/create-timeslot.dto';
 @ValidatorConstraint({ async: false })
 class IsUniqueTimeSlotsConstraint implements ValidatorConstraintInterface {
   validate(timeSlots: TimeSlotDto[]) {
-    return plainToInstance(TimeSlotDto, timeSlots).reduce(
-      (acc, timeSlot) => {
-        if (acc.seenDates.includes(timeSlot.start.toDateString())) {
-          acc.isValid = false;
-        } else {
-          acc.seenDates.push(timeSlot.start.toDateString());
-        }
-        return acc;
-      },
-      { seenDates: [], isValid: true },
-    ).isValid;
+    const isValidTimeSlot = (timeslot: TimeSlotDto): boolean => {
+      return (
+        timeslot.start.toDateString() === timeslot.end.toDateString() &&
+        timeslot.start.getTime() < timeslot.end.getTime() &&
+        timeslot.end.getTime() - timeslot.start.getTime() >= 60 * 60 * 1000 &&
+        timeslot.start.getMinutes() === 0 &&
+        timeslot.end.getMinutes() === 0
+      );
+    };
+
+    return (
+      timeSlots
+        .map((slot) => plainToInstance(TimeSlotDto, slot))
+        .sort((a, b) => a.start.getTime() - b.start.getTime())
+        .filter((slot, i, arr) => {
+          if (!isValidTimeSlot(slot)) {
+            return true;
+          }
+          if (i > 0) {
+            const prevSlot = arr[i - 1];
+            if (prevSlot.end.getTime() > slot.start.getTime()) {
+              return true;
+            }
+          }
+
+          return false;
+        }).length === 0
+    );
   }
 
   defaultMessage() {
-    return 'Each day can have only one timeslot.';
+    return (
+      'Each timeslot instance must meet the following conditions: ' +
+      '1. is within one day; ' +
+      '2. start time must be before end time; ' +
+      '3. start and end must be expressed in hours without minutes; ' +
+      '4. lasting at least 1 hour; ' +
+      '5. all timeslots should not overlap.'
+    );
   }
 }
 
